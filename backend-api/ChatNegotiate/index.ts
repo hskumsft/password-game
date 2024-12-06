44a8c47a1da840bc2bb1dc047fc7bdd13dd9f1ae
Nash Kuo <hsku@microsoft.com>
2024-12-06 10:31:19 -0800

    migrate to v4 model for chatNegotiate

--- backend-api/ChatNegotiate/index.ts
+++ backend-api/ChatNegotiate/index.ts
@@ -1,3 +1,17 @@
-export default async function (context, req, connectionInfo) {
-    context.res.body = connectionInfo;
-};
+const { app, input } = require('@azure/functions');
+
+const inputSignalR = input.generic({
+    type: 'signalRConnectionInfo',
+    name: 'connectionInfo',
+    hubName: 'chat',
+    connectionStringSetting: 'SignalRConnectionString',
+});
+
+app.post('chatNegotiate', {
+    authLevel: 'anonymous',
+    handler: (request, context) => {
+        return { body: JSON.stringify(context.extraInputs.get(inputSignalR)) }
+    },
+    route: 'chat/negotiate',
+    extraInputs: [inputSignalR],
+});
