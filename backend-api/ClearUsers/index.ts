5d6f0c93c486ad6fbe35bf338d54fe1e5051c62e
Nash Kuo <hsku@microsoft.com>
2024-12-06 15:01:50 -0800

    migrate to v4 for clearUsers

--- backend-api/ClearUsers/index.ts
+++ backend-api/ClearUsers/index.ts
@@ -1,18 +1,19 @@
-import { AzureFunction, Context } from "@azure/functions"
+import { app } from "@azure/functions";
 import { TableClient } from "@azure/data-tables";
 import { UserTableName, DefaultOperationOptions } from "../Settings";
-
-const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
-    const tableClient = TableClient.fromConnectionString(
-        process.env["StorageAccountConnectionString"], UserTableName);
-
-    try {
-        await tableClient.deleteTable(DefaultOperationOptions)
-    } catch (err) {
-        if (err.details?.odataError?.code !== "ResourceNotFound") {
-            throw err;
+ 
+app.timer('clearUsers', {
+    schedule: '0 0 8 * * *',
+    handler: async (myTimer, context) => {
+        const tableClient = TableClient.fromConnectionString(
+            process.env["StorageAccountConnectionString"], UserTableName);
+    
+        try {
+            await tableClient.deleteTable(DefaultOperationOptions)
+        } catch (err) {
+            if (err.details?.odataError?.code !== "ResourceNotFound") {
+                throw err;
+            }
         }
-    }
-};
-
-export default timerTrigger;
+    },
+});
