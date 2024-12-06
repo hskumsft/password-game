9b97803d25c9acd733f66ec941e41700f4d41e5d
Nash Kuo <hsku@microsoft.com>
2024-12-06 15:54:32 -0800

    migrate to v4 for updateUser

--- backend-api/UpdateUser/index.ts
+++ backend-api/UpdateUser/index.ts
@@ -1,46 +1,9 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { TableClient } from "@azure/data-tables";
 import Profanity from "profanity-js";
 import { UserWithSecurityAttrs, UserToTableEntity } from "@pwdgame/shared";
 import { UserTableName, DefaultOperationOptions } from "../Settings";
 
-const updateUser: AzureFunction = async (context: Context, req: HttpRequest) => {
-    if (!isValidUser(req.body)) {
-        context.res = {
-            status: 400,
-            body: "Invalid user object"
-        };
-        return;
-    }
-
-    const profanity = new Profanity('', { language: 'en-us' })
-    if (profanity.isProfane(req.body.username)) {
-        context.res = {
-            status: 409,
-            body: "Username contains banned words"
-        };
-        return;
-    }
-
-    const entity = UserToTableEntity(req.body);
-
-    const tableClient = TableClient.fromConnectionString(
-        process.env["StorageAccountConnectionString"], UserTableName);
-
-    try {
-        await tableClient.createTable(DefaultOperationOptions);
-    } catch (err) {
-        if (err.details?.odataError?.code !== "TableAlreadyExists") {
-            throw err;
-        }
-    }
-
-    await tableClient.upsertEntity(entity, "Merge", DefaultOperationOptions);
-
-    context.res = {
-        status: 204 // no content
-    };
-};
+const { app } = require('@azure/functions');
 
 const isValidUser = (user: any): user is UserWithSecurityAttrs => {
     return user
@@ -50,4 +13,43 @@ const isValidUser = (user: any): user is UserWithSecurityAttrs => {
         && (user as {}).hasOwnProperty("securityAnswers");
 }
 
-export default updateUser;
+app.http('updateUser', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        const requestBody = await request.json();
+
+        if (!isValidUser(requestBody)) {
+            return {
+                status: 400,
+                body: "Invalid user object"
+            };
+        }
+
+        const profanity = new Profanity('', { language: 'en-us' })
+        if (profanity.isProfane(requestBody.username)) {
+            return {
+                status: 409,
+                body: "Username contains banned words"
+            };
+        }
+        
+        const entity = UserToTableEntity(requestBody);
+        const tableClient = TableClient.fromConnectionString(
+            process.env["StorageAccountConnectionString"], UserTableName);
+    
+        try {
+            await tableClient.createTable(DefaultOperationOptions);
+        } catch (err) {
+            if (err.details?.odataError?.code !== "TableAlreadyExists") {
+                throw err;
+            }
+        }
+    
+        await tableClient.upsertEntity(entity, "Merge", DefaultOperationOptions);
+    
+        return {
+            status: 204 // no content
+        };
+    },
+});
